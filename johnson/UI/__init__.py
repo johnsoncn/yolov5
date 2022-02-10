01df1f4cf007e84b86124f28daa06502fe87d43b
丁超凡 <dingchaofan@corp.netease.com>
2022-02-10 17:04:28 +0800

    update

---
+++ johnson/UI/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+""" 
+@Time    : 2022/2/10 15:40
+@Author  : Johnson
+@FileName: __init__.py.py
+"""
---
+++ johnson/UI/test.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+""" 
+@Time    : 2022/2/10 17:02
+@Author  : Johnson
+@FileName: test.py
+"""
+import random
+if 0 == True:
+    print('aaa')
+else:
+    print('bbb')
+
+
+print(random.random())
